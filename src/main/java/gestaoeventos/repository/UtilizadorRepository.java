1aae7fdeeef1fd5e902b9200d8a926bc22e0eb58
Rodrigo Barbosa <rb6544758@gmail.com>
2025-12-11 17:36:46 +0000

    remove unused documentation comments from UtilizadorRepository

--- src/main/java/gestaoeventos/repository/UtilizadorRepository.java
+++ src/main/java/gestaoeventos/repository/UtilizadorRepository.java
@@ -5,15 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-/**
- * Repositório para a entidade Utilizador.
- */
 public interface UtilizadorRepository extends JpaRepository<Utilizador, Integer> {
 
-    /**
-     * Procura um utilizador pelo email.
-     *
-     * Retorna um Optional porque o utilizador pode não existir
-     */
     Optional<Utilizador> findByEmail(String email);
 }
+
